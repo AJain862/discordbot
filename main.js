20e589e5df0010cc39ac08e3ec16a37960c5b4c8
AJain862 <72466106+AJain862@users.noreply.github.com>
2020-10-08 16:44:31 -0700

    g

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ const Discord = require('discord.js')
 const client = new Discord.Client()
 
 const config = require('./config.json')
-
+const command = require('./command')
 const welcome = require('./welcome')
 const { dir } = require('console')
 

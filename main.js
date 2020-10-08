cc52de481c50a5f8cf8a8afb59664fff7e01995b
AJain862 <72466106+AJain862@users.noreply.github.com>
2020-10-08 16:52:00 -0700

    j

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ const client = new Discord.Client()
 const config = require('./config.json')
 const command = require('./command')
 const welcome = require('./welcome')
-const { dir } = require('console')
+
 
 
 
@@ -21,19 +21,21 @@ client.on('ready', async () => {
   const baseFile = 'command-base.js'
   const commandBase = require(`./commands/${baseFile}`)
 
-  const readCommands = dir => {
-      const files = fs.readdirSync(path.join(__dirname, dir))
-      for (const file of files) {
-          const stat = fs.lstatSync(path.join(__dirname, dir, file))
-          if (stat.isDirectory()) {
-              readCommands(path.join(dir, file))
-          } else if (file !== baseFile) {
-            const option = require(path.join(__dirname, dir, file))
-            commandBase(client, option)
-
-          }
+  const readCommands = (dir) => {
+    const files = fs.readdirSync(path.join(__dirname, dir))
+    for (const file of files) {
+      const stat = fs.lstatSync(path.join(__dirname, dir, file))
+      if (stat.isDirectory()) {
+        readCommands(path.join(dir, file))
+      } else if (file !== baseFile) {
+        const option = require(path.join(__dirname, dir, file))
+        commandBase(client, option)
       }
+    }
   }
+
+  readCommands('commands')
+
 
   welcome(client)
 

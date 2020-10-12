6ad1bacb347163b33f13095153f00d88976c65f6
AJain862 <72466106+AJain862@users.noreply.github.com>
2020-10-12 10:50:50 -0700

    g

--- welcome.js
+++ welcome.js
@@ -1,6 +1,6 @@
 module.exports = client => {
-    const channelId = '749999326805098576'
-    const targetChannelId = '749998454817816666'
+    const channelId = '765259740590374927'
+    const targetChannelId = '765259740590374925'
     client.on('guildMemberAdd', (member) => {
 
         const message = `Please welcome <@${member.id}> to the server! Please check ${member.guild.channels.cache.get(targetChannelId).toString()}`

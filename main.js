756a53af419e84e4a7e4204d7b6bd5c20c00728d
AJain862 <72466106+AJain862@users.noreply.github.com>
2020-10-12 11:56:25 -0700

    g

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
+const path = require('path')
 
+const fs = require('fs')
 
 
 const Discord = require('discord.js')
@@ -17,7 +19,23 @@ const roleClaim = require('./role-claim')
 client.on('ready', async () => {
   console.log('The client is ready!')
 
+  const baseFile = 'command-base.js'
+  const commandBase = require(`./commands/${baseFile}`)
 
+  const readCommands = (dir) => {
+    const files = fs.readdirSync(path.join(__dirname, dir))
+    for (const file of files) {
+      const stat = fs.lstatSync(path.join(__dirname, dir, file))
+      if (stat.isDirectory()) {
+        readCommands(path.join(dir, file))
+      } else if (file !== baseFile) {
+        const option = require(path.join(__dirname, dir, file))
+        commandBase(client, option)
+      }
+    }
+  }
+
+  readCommands('commands')
 
 
   welcome(client)

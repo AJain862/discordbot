dd173e04d3777d54a657af071ffcb87d395f7f8d
AJain862 <72466106+AJain862@users.noreply.github.com>
2020-10-12 16:00:09 -0700

    g

--- commands/user-info.js
+++ commands/user-info.js
@@ -1,6 +1,6 @@
 module.exports = {
     commands: ['userinfo', 'whois'],
-    expectedArgs: '<num1> <num2>',
+    
     permissionError: 'You need admin permissions to run this command',
     minArgs: 1,
     maxArgs: 1,

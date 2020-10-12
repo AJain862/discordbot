6710a1a68757dbdaf33a780e81e7adab94c88c15
AJain862 <72466106+AJain862@users.noreply.github.com>
2020-10-12 14:29:40 -0700

    g

--- role-claim.js
+++ role-claim.js
@@ -1,3 +1,4 @@
+const add = require('./commands/add')
 const firstMessage = require('./first-message')
 module.exports = (client) => {
     const channelId = '765289064772927490'
@@ -11,7 +12,7 @@ module.exports = (client) => {
 
     const reactions = []
 
-    let emojiText = 'pls add a reaction to claim a role'
+    let emojiText = 'pls add a reaction to claim a role\n\n'
     for (const key in emojis) {
         const emoji = getEmoji(key)
         reactions.push(emoji)
@@ -22,6 +23,44 @@ module.exports = (client) => {
 
 
     firstMessage(client, channelId, emojiText, reactions )
+
+    const handleReaction = (reaction,user,add) => {
+        if (user.id === '761283809144471552') {
+            return
+        }
+        const emoji = reaction._emoji.name
+
+        const { guild } = reaction.message
+
+        const roleName = emojis[emoji]
+        if(!roleName) {
+            return
+        }
+
+        const role = guild.roles.cache.find(role => role.name === roleName)
+        const member = guild .members.cache.find(memer => member .id === user.id) 
+
+        if (add) {
+            member.roles.add(role)
+        }
+        else {
+            member.roles.add(role)
+        }
+
+    }
+
+    client.on('messageReactionAdd', (reaction, user) => {
+        if(reaction.message.channel.id === channelId) {
+            handleReaction(reaction, user, true)
+        }
+
+    })
+
+    client.on('messageReactionRemove', (reaction, user) => {
+        if(reaction.message.channel.id === channelId) {
+            handleReaction(reaction, user, false)
+        }
+    })
 
 }
 

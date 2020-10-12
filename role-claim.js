aa41d9bb29de464fe4492bfb499b7390e6a73632
AJain862 <72466106+AJain862@users.noreply.github.com>
2020-10-12 14:08:46 -0700

    g

--- role-claim.js
+++ role-claim.js
@@ -1,62 +1,28 @@
 const firstMessage = require('./first-message')
-
 module.exports = (client) => {
-  const channelId = '750032729512476682'
+    const channelId = '750032729512476682'
 
-  const getEmoji = (emojiName) =>
-    client.emojis.cache.find((emoji) => emoji.name === emojiName)
+    const getEmoji = (emojiName) => client.emojis.cache.find(emoji => emoji.name === emojiName)
 
-  const emojis = {
-    uno: 'Guest',
-    
-  }
-
-  const reactions = []
-
-  let emojiText = 'Add a reaction to claim a role\n\n'
-  for (const key in emojis) {
-    const emoji = getEmoji(key)
-    reactions.push(emoji)
-
-    const role = emojis[key]
-    emojiText += `${emoji} = ${role}\n`
-  }
-
-  firstMessage(client, channelId, emojiText, reactions)
-
-  const handleReaction = (reaction, user, add) => {
-    if (user.id === '723819104045105172') {
-      return
+    const emojis = {
+        uno: 'Guest'
+        
     }
 
-    const emoji = reaction._emoji.name
+    const reactions = []
 
-    const { guild } = reaction.message
+    let emojiText = ''
+    for (const key in emojis) {
+        const emoji = getEmoji(key)
+        reactions.push(emoji)
 
-    const roleName = emojis[emoji]
-    if (!roleName) {
-      return
+        const role = emojis[key]
+        emojiText += `${emoji} = ${role}\n`
     }
 
-    const role = guild.roles.cache.find((role) => role.name === roleName)
-    const member = guild.members.cache.find((member) => member.id === user.id)
 
-    if (add) {
-      member.roles.add(role)
-    } else {
-      member.roles.remove(role)
-    }
-  }
+    firstMessage(client, channelId, emojiText, [])
 
-  client.on('messageReactionAdd', (reaction, user) => {
-    if (reaction.message.channel.id === channelId) {
-      handleReaction(reaction, user, true)
-    }
-  })
-
-  client.on('messageReactionRemove', (reaction, user) => {
-    if (reaction.message.channel.id === channelId) {
-      handleReaction(reaction, user, false)
-    }
-  })
 }
+
+            

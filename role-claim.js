de2b97b313f7429c7823a836428ad77550ff2024
AJain862 <72466106+AJain862@users.noreply.github.com>
2020-10-12 12:24:55 -0700

    g

--- role-claim.js
+++ role-claim.js
@@ -2,19 +2,19 @@ const firstMessage = require('./first-message')
 module.exports = (client) => {
     const channelId = '765289064772927490'
 
-    const getEmoji = emojiName => client.emojis.cache.find(emoji => emoji.name === emojiName)
+    const getEmoji = (emojiName) => client.emojis.cache.find(emoji => emoji.name === emojiName)
 
     const emojis = {
-        memeber: '[—Community Members—]',
+        memeber: '[—Community Members—]'
         
     }
 
-    const reactionIds = []
+    const reactions = []
 
     let emojiText = ''
     for (const key in emojis) {
         const emoji = getEmoji(key)
-        reactionIds.push(emoji)
+        reactions.push(emoji)
 
         const role = emojis[key]
         emojiText += `${emoji} = ${role}\n`

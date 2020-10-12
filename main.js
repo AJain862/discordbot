2b9b770aef389092cc08bd322e61b796b81d1e45
AJain862 <72466106+AJain862@users.noreply.github.com>
2020-10-12 16:10:35 -0700

    g

--- main.js
+++ main.js
@@ -146,6 +146,47 @@ client.on('ready', async () => {
     
   })
 
+  command(client, 'userinfo', (message) => {
+    const { guild, channel } = message
+    //console.log(guild)
+
+    const user = message.mentions.users.first() || message.member.user
+    const member = guild.members.cache.get(user.id)
+
+    const embed = new Discord.MessageEmbed()
+    .setAuthor(`User info for ${user.username}`, user.displayAvatarURL())
+    .addFields(
+      {
+        name: 'User tag',
+        value: user.tag,
+      },
+      {
+        name: 'Is bot',
+        value: user.bot,
+      },
+      {
+        name: 'Nickname',
+        value: member.nickname || 'None',
+      },
+      {
+        name: 'Joined Server',
+        value: new Date(member.joinedTimestamp).toLocaleDateString(),
+      },
+      {
+        name: 'Joined Discord',
+        value: new Date(user.createdTimestamp).toLocaleDateString(),
+      },
+      {
+        name: 'Roles',
+        value: member.roles.cache.size - 1,
+      }
+    )
+
+
+    channel.send(embed)
+    
+  })
+
   command(client, ['ping', 'test'], (message) => {
     message.channel.send(`PONG! ${client.ws.ping}ms`)
   })

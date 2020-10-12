739b4d29394815fa12f2bd9b13f6a6fe258d8342
AJain862 <72466106+AJain862@users.noreply.github.com>
2020-10-12 11:37:29 -0700

    g

--- role-claim.js
+++ role-claim.js
@@ -1,5 +1,5 @@
 const firstMessage = require('./first-message')
-module.exports = client => {
+module.exports = (client) => {
     const channelId = '765280692455211011'
 
     firstMessage(client, channelId, 'hello world', [])

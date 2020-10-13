08868516367e2b003064dd919f4595b6d599a086
AJain862 <72466106+AJain862@users.noreply.github.com>
2020-10-13 15:50:30 -0700

    f

--- commands/lock.js
+++ commands/lock.js
@@ -1,38 +1,37 @@
 module.exports = {
-    commands: ['lock'],
-    
+    command: ['lock'],
+
 
     permissionError: 'You need admin permissions to run this command',
-    minArgs: 1,
-    maxArgs: 1,
-    
+
+
     callback: (message, arguments, text) => {
         async function f() {
-        if(arguments[0]) return message.channel.send('You did not mention any channels');
-        if(!message.mentions.channels.first()) return message.channel.send('You did not mention a channel.')
+            if (arguments[0]) return message.channel.send('You did not mention any channels');
+            if (!message.mentions.channels.first()) return message.channel.send('You did not mention a channel.')
 
-        const role = message.guild.roles.get('765356807895384072');
-        if(!role) return message.channel.send('Role is not found');
+            const role = message.guild.roles.get('765356807895384072');
+            if (!role) return message.channel.send('Role is not found');
 
-        await message.mentions.channels.forEach(async channel => {
-            if (channel.name.startsWith('🔒')) return message.channel.send(`<#${channel.id}> is already locked`);
-            await channel.setName(`🔒${channel.name}`);
-            try {
-                channel.overwritesPermissions(role, {
-                    SEND_MESSAGES: false
-                });
-                message.channel.send(`<#${channel.id}> has been locked`)
+            await message.mentions.channels.forEach(async channel => {
+                if (channel.name.startsWith('🔒')) return message.channel.send(`<#${channel.id}> is already locked`);
+                await channel.setName(`🔒${channel.name}`);
+                try {
+                    channel.overwritesPermissions(role, {
+                        SEND_MESSAGES: false
+                    });
+                    message.channel.send(`<#${channel.id}> has been locked`)
 
-            }catch (err) {
-                console.log(err)
-                message.channel.send('Something has gone wrong locking the channels');
-            }
-        })
+                } catch (err) {
+                    console.log(err)
+                    message.channel.send('Something has gone wrong locking the channels');
+                }
+            })
 
-        
-    }
+
+        }
     },
-    permissions: 'ADMINISTRATOR', 
-    
-  }
+    permissions: 'ADMINISTRATOR',
+
+}
 
--- commands/unlock.js
+++ commands/unlock.js
@@ -1,38 +1,37 @@
 module.exports = {
-    commands: ['unlock'],
-    
-    
+    command: ['unlock'],
+
+
     permissionError: 'You need admin permissions to run this command',
-    minArgs: 1,
-    maxArgs: 1,
-    
-    callback: (message, arguments, text, ) => {
+
+
+    callback: (message, arguments, text,) => {
         async function f() {
-        
-        if(arguments[0]) return message.channel.send('You did not mention any channels');
-        if(!message.mentions.channels.first()) return message.channel.send('You did not mention a channel.')
 
-        const role = message.guild.roles.get('765356807895384072');
-        if(!role) return message.channel.send('Role is not found');
+            if (arguments[0]) return message.channel.send('You did not mention any channels');
+            if (!message.mentions.channels.first()) return message.channel.send('You did not mention a channel.')
 
-        await message.mentions.channels.forEach(async channel => {
-            if (!channel.name.startsWith('🔒')) return message.channel.send(`<#${channel.id}> is already unlocked`);
-            await channel.setName(channel.name.substring(1));
-            try {
-                channel.overwritesPermissions(role, {
-                    SEND_MESSAGES: true
-                });
-                message.channel.send(`<#${channel.id}> has been unlocked`)
+            const role = message.guild.roles.get('765356807895384072');
+            if (!role) return message.channel.send('Role is not found');
 
-            }catch (err) {
-                console.log(err)
-                message.channel.send('Something has gone wrong unlocking the channels');
-            }
-        })
+            await message.mentions.channels.forEach(async channel => {
+                if (!channel.name.startsWith('🔒')) return message.channel.send(`<#${channel.id}> is already unlocked`);
+                await channel.setName(channel.name.substring(1));
+                try {
+                    channel.overwritesPermissions(role, {
+                        SEND_MESSAGES: true
+                    });
+                    message.channel.send(`<#${channel.id}> has been unlocked`)
 
-        
-    }
+                } catch (err) {
+                    console.log(err)
+                    message.channel.send('Something has gone wrong unlocking the channels');
+                }
+            })
+
+
+        }
     },
-    permissions: 'ADMINISTRATOR', 
-    
-  }
+    permissions: 'ADMINISTRATOR',
+
+}

c3a7b1d67a61721130a12101b35781a09f52ba7d
AJain862 <72466106+AJain862@users.noreply.github.com>
2020-10-13 08:02:52 -0700

    Update welcome.js

--- welcome.js
+++ welcome.js
@@ -1,12 +1,18 @@
+const { DiscordAPIError } = require("discord.js")
+
 module.exports = client => {
     const channelId = '765356808172208154'
     const targetChannelId = '765356808172208152'
+    
     client.on('guildMemberAdd', (member) => {
+        
 
         const message = `Please welcome <@${member.id}> to the server! Please check ${member.guild.channels.cache.get(targetChannelId).toString()}`
-
+        
+        
         const channel = member.guild.channels.cache.get(channelId)
         channel.send(message)
+        channel.send(embed)
 
     })
 

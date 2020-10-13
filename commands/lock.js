808e3e961335cfad2fd053ef0db32077a4e77bdf
AJain862 <72466106+AJain862@users.noreply.github.com>
2020-10-13 15:41:10 -0700

    s

--- commands/lock.js
+++ commands/lock.js
@@ -3,8 +3,8 @@ module.exports = {
     
 
     permissionError: 'You need admin permissions to run this command',
-    minArgs: 1,
-    maxArgs: 1,
+    minArgs: 2,
+    maxArgs: 2,
     
     callback: (message, arguments, text) => {
         async function f() {
--- commands/unlock.js
+++ commands/unlock.js
@@ -4,7 +4,7 @@ module.exports = {
     
     permissionError: 'You need admin permissions to run this command',
     minArgs: 2,
-    maxArgs: 1,
+    maxArgs: 2,
     
     callback: (message, arguments, text, ) => {
         async function f() {

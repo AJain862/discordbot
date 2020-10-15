2e69ecf83fa2d1dd4685c5ab7b7e72781b681426
AJain862 <72466106+AJain862@users.noreply.github.com>
2020-10-15 09:34:57 -0700

    f

--- welcome.js
+++ welcome.js
@@ -4,10 +4,13 @@ const { DiscordAPIError } = require("discord.js")
 module.exports = client => {
     const channelId = '765356808172208154'
     const targetChannelId = '765356808172208152'
+    let avatar = user.displayAvatarURL()
     
     
     
     client.on('guildMemberAdd', (member) => {
+        let embed = new Discord.MessageEmbed()
+            .setThumbnail(avatar)
 
         
         

b289d96fbbe1eba53738d1cb47f5b0cab510509a
AJain862 <72466106+AJain862@users.noreply.github.com>
2020-10-14 18:05:21 -0700

    f

--- commands/mute.js
+++ commands/mute.js
@@ -4,8 +4,8 @@ module.exports = {
     name: 'mute',
     description: 'mutes a user',
     permssionError: 'You need admin perms to run this command',
-    minArgs: 1,
-    maxArgs: 1,
+    minArgs: 0,
+    maxArgs: 0,
     callback: (message, arguements, text => {
 
         message.channel.send('hi')

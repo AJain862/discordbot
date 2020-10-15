8f3c6aff82c3fa35826da4970816eae24235d018
AJain862 <72466106+AJain862@users.noreply.github.com>
2020-10-15 09:36:59 -0700

    f

--- welcome.js
+++ welcome.js
@@ -22,6 +22,7 @@ module.exports = client => {
         
         const channel = member.guild.channels.cache.get(channelId)
         channel.send(message)
+        channel.send(embed)
         
 
     })

b1d3f461c396f79d4fb41ec0928afbf8fa5231f1
AJain862 <72466106+AJain862@users.noreply.github.com>
2020-10-16 11:15:12 -0700

    f

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ const { Random } = require("something-random-on-discord")
 
 
 
+const translate = require('@k3rn31p4nic/google-translate-api');
 
 const Discord = require('discord.js')
 const client = new Discord.Client()
@@ -437,7 +438,28 @@ client.on("message", async message => {
       message.channel.send(data)
     }
   });
-
+client.on('message', async message => {
+    const { MessageEmbed } = require('discord.js');
+    let args = message.content.slice(prefix.length).split(" ");
+    let cmd = args.shift().toLowerCase();
+    if(cmd === 'slow') {
+        try {
+            if (args.length < 2) {
+              return message.reply("Command Usage: `translate <Language> <Text>`")
+            }
+      
+            const result = await translate(args.slice(1).join(' '), { to: args[0] });
+      
+            const embed = new MessageEmbed()
+              .setColor('#68b64a')
+              .setDescription(result.text)
+              .setFooter(`Translation from ${result.from.language.iso.toUpperCase()} to ${args[0].toUpperCase()}`);
+            message.channel.send({ embed });
+          } catch (err) {
+            return message.reply(`Oh no, an error occurred: \`${err.message}\`.`);
+          }
+        }
+})
 
 /*client.on('message', (message) => {
     const { mentions, member } = message

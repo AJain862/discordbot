41eaf6e0595d432951231aa98dca9774593c6ead
AJain862 <72466106+AJain862@users.noreply.github.com>
2020-10-18 21:54:31 -0700

    f

--- welcome.js
+++ welcome.js
@@ -10,12 +10,24 @@ module.exports = (client) => {
         const { member, channel, content, guild } = message
         
         if (!member.hasPermission('ADMINISTRATOR')) return channel.send('You do not have permission to use this command.')
+        let text = content
+        const split = text.split(' ')
+
+        if(split.length < 2) {
+            channel.send('Please provide a welcome message')
+            return
+        }
+
+        split.shift()
+
+        text = split.join(' ')
+
         await mongo().then(async(mongoose) => {
             try{
                 await new welcomeSchema({
                     _id: guild.id,
                     channelId: channel.id,
-                    text: content,
+                    text: text,
                 }).save()
 
 

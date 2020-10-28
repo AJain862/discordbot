6e0428dc5398f255f15298ed191bc8c72a8b7b50
AJain862 <72466106+AJain862@users.noreply.github.com>
2020-10-27 23:20:47 -0700

    Update main.js

--- main.js
+++ main.js
@@ -668,7 +668,7 @@ client.on("message", async message => {
 
     if(cmd === "a-roleadd") {
         
-        let rolename = message.guild.roles.cache.find(x => x.name === args[0]);
+        let rolename = message.guild.roles.cache.find(x => x.name === args[1]);
 
         message.channel.send(rolename)
     

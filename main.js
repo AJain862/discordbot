7c70c9e4e97e3b4a0fe6ab1cb94fe8b3496870e6
AJain862 <72466106+AJain862@users.noreply.github.com>
2020-11-02 16:23:29 -0800

    Update main.js

--- main.js
+++ main.js
@@ -39,6 +39,7 @@ const { join } = require('path')
 client.on('ready', async () => {
   console.log('The client is ready!')
   client.user.setStatus('dnd')
+  client.user.setPresence('a-help')
   await mongo().then(mongoose => {
       try {
           console.log('Connected to mongo!')
